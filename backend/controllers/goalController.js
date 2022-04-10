34c20307c0efa3a39834a5c40f01a5bbf8ab1c72
shedrachIkenna <shedrach686@gmail.com>
2022-04-10 22:02:21 +0100

    Added controller functions for requests and middleware for body data

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,40 @@
+//@description - Get goals
+//@route -       GET /api/goals
+//access =       Private
+const getGoal = (req, res) => {
+    res.status(200).json({message: 'Get goals' })
+}
+
+//@description - Set goal
+//@route -       POST /api/goals
+//access =       Private
+const setGoal = (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+    res.status(200).json({ message: 'Set goal'})
+}
+
+//@description - Update goal
+//@route -       PUT /api/goals/:id
+//access =       Private
+const updateGoal = (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}`})
+}
+
+//@description - Delete goal
+//@route -       DELETE /api/goals/:id
+//access =       Private
+const deleteGoal = (req, res) => {
+    res.status(200).json({ message: `Deleta goal ${req.params.id}`})
+}
+
+
+module.exports = {
+    getGoal,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+}
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,21 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const { getGoal, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Get goals'})
-})
-
-router.post('/', (req, res) => {
-    res.status(200).json({ message: 'Set goal'})
-})
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}`})
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ message: `Deleta goal ${req.params.id}`})
-})
-
+router.route('/').get(getGoal).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
