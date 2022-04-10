76b7df4456374dc2fc5656aa644cd8452b92dbc0
shedrachIkenna <shedrach686@gmail.com>
2022-04-10 20:55:57 +0100

    created routes in seperate folder

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,5 +5,17 @@ router.get('/', (req, res) => {
     res.status(200).json({ message: 'Get goals'})
 })
 
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'Set goal'})
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}`})
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ message: `Deleta goal ${req.params.id}`})
+})
+
 
 module.exports = router
